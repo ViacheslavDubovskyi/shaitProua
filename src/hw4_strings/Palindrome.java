08f7d873142bd77df105ec30b0652b85af65a344
Viacheslav Dubovskyi <architect1slava@gmail.com>
2025-02-07 15:13:43 +0200

    hw4 add Palindrome

---
+++ src/hw4_strings/Palindrome.java
@@ -0,0 +1,28 @@
+package hw4_strings;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+
+        System.out.println(isPalindrome(str));
+
+    }
+
+    public static boolean isPalindrome(String str) {
+        boolean isPalindrome = false;
+        String reversedString = "";
+        for (int i = str.length() - 1; i > -1; i--) {
+            reversedString += str.charAt(i);
+        }
+        if (str.equals(reversedString)) {
+            isPalindrome = true;
+        }
+
+        return isPalindrome;
+    }
+}
