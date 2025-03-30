a37e6cddfca57f9a3f303a922088fdd5c8112b44
Viacheslav Dubovskyi <architect1slava@gmail.com>
2025-03-31 00:40:48 +0300

    hw9 add TwoSum

--- src/hw9_tasks/TwoSum.java
+++ src/hw9_tasks/TwoSum.java
@@ -1,16 +1,32 @@
 package hw9_tasks;
 
+import java.util.Arrays;
+
 public class TwoSum {
 
     public static void main(String[] args) {
 
         int[] nums1 = {2, 7, 11, 15};
         int target1 = 9;
+        System.out.println(Arrays.toString(twoSum(nums1, target1)));
 
         int[] nums2 = {3, 2, 4};
         int target2 = 6;
+        System.out.println(Arrays.toString(twoSum(nums2, target2)));
 
         int[] nums3 = {3, 3};
         int target3 = 6;
+        System.out.println(Arrays.toString(twoSum(nums3, target3)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
     }
 }
