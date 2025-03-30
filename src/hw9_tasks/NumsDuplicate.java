f5e472e1a23be840ea88cc3ecfc4277a245e79df
Viacheslav Dubovskyi <architect1slava@gmail.com>
2025-03-30 23:30:42 +0300

    hw9 add NumsDuplicate

--- src/hw9_tasks/NumsDuplicate.java
+++ src/hw9_tasks/NumsDuplicate.java
@@ -1,5 +1,9 @@
 package hw9_tasks;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class NumsDuplicate {
 
     public static void main(String[] args) {
@@ -7,5 +11,15 @@ public class NumsDuplicate {
         int[] nums1 = {1, 2, 3, 1};
         int[] nums2 = {1, 2, 3, 4};
         int[] nums3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+        System.out.println(checkDuplicate(nums1));
+        System.out.println(checkDuplicate(nums2));
+        System.out.println(checkDuplicate(nums3));
+    }
+
+    public static boolean checkDuplicate(int[] nums) {
+        Set<Integer> unique = new HashSet<>();
+        Arrays.stream(nums).forEach(unique::add);
+        return !(unique.size() == nums.length);
     }
 }
